20c1e3a50185aca389324e7ba335dcb9096fc11e
Felipe <afelipe.freitas@gmail.com>
2023-09-13 22:27:20 +0200

    start project and read file

---
+++ index.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+function main() {
+    fs.readFile('./fi.rinha.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const ast = JSON.parse(data);
+        console.log(ast);
+    });
+}
+main();
